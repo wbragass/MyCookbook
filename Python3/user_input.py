ddadb9f2c1250ae17ef8d1e242d237149cc4e54c
william bragassa <william.bragassa@yahoo.com>
2019-09-26 12:33:19 -0600

    Add user input and variable types cookbooks

---
+++ Python3/user_input.py
@@ -0,0 +1,6 @@
+print("Hello! \n")
+user_name=input("What is your name? ")
+print("Hello " + user_name)
+
+user_number=int(input("What is your favorite number? "))
+print("Your favorite number is: " + str(3*user_number))
---
+++ Python3/variable_types.py
@@ -0,0 +1,7 @@
+my_integer = 3
+my_float = 3.141592
+my_string = "hello"
+
+print(type(my_integer))
+print(type(my_float))
+print(type(my_string))
